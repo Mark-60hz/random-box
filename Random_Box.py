3ec6c04a88d08752675d76ad3a833692734d717b
21zhichua <91635596+21zhichua@users.noreply.github.com>
2021-10-31 08:00:27 -0700

    Update Random_Box.py

--- Random_Box.py
+++ Random_Box.py
@@ -4,12 +4,31 @@ t.fillcolor('orange')
 t.pensize(5)
 t.turtlesize(10)
 t.shape("square")
+def spot_clicked(x,y):
+  change_position()
+  update_score()
+def change_position():
+  new_xpos = rand.randint(-180,180)
+  new_ypos = rand.randint(-140,140)
+  t.hideturtle()
+  t.goto(new_xpos,new_ypos)
+  t.showturtle()
+t.onclick(spot_clicked)
 
-def fxn(x,y): 
-  wn.addshape('scary.gif')
-  t.shape('scary.gif') 
+def fxn1(x,y): 
+  wn.addshape('scary1.gif')
+  t.shape('scary1.gif')
+def fxn2(x,y):
+  wn.addshape('scary2.gif')
+  t.shape('scary2.gif')
+  
+def fxn3(x,y):
+  wn.addshape('scary3.gif')
+  t.shape('scary3.gif')
 
-t.onclick(fxn)
+t.onclick(fxn1)
+t.onclick(fxn2)
+t.onclick(fxn3)
 
 wn = t.Screen()
 wn.mainloop()
